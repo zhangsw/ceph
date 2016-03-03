a3849c1f3322a2dec0156f61fc430db03748dfad
John Spray <john.spray@redhat.com>
2016-03-03 11:59:21 +0000

    libcephfs: fix crash on getpwd
    ...if the directory has been unlinked in
    the background.  Previously this would
    assert out, now it will just give you
    the last string that you passed into chdir.
    
    Signed-off-by: John Spray <john.spray@redhat.com>

--- src/client/Client.h
+++ src/client/Client.h
@@ -884,7 +884,7 @@ public:
   int statfs(const char *path, struct statvfs *stbuf);
 
   // crap
-  int chdir(const char *s);
+  int chdir(const char *s, std::string &new_cwd);
   void getcwd(std::string& cwd);
 
   // namespace ops
